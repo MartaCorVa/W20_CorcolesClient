a604aaa5f49b0a286fa508577a827fdefea15535
Marta <martacorcolesvalle@gmail.com>
2020-02-21 18:59:36 +0100

    first commit

---
+++ Assets/Scripts/MainMenu/ShowPlayerName.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPlayerName : MonoBehaviour
+{
+    [SerializeField]
+    public GameManager gameManager;
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        player.Name = gameManager.player.Name;
+    }
+}
